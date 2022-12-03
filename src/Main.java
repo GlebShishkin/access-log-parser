8e0ad9feb7eadb38353e92ed4ed5262949307203
Gleb Shishkin <GSHishkin@inno.tech>
2022-12-03 19:30:04 +0300

    Добавлен

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-    }
+            int N = 0;
+
+            while(true) {
+                System.out.println("Укажите путь к файлу:");
+                String path = new Scanner(System.in).nextLine();
+                File file = new File(path);
+                boolean fileExists = file.exists();
+                boolean isDirectory = file.isDirectory();
+
+                if (fileExists && !isDirectory) {
+                    System.out.println("Путь указан верно");
+                } else if (!fileExists) {
+                    System.out.println("файл не существует");
+                    continue;
+                } else if (isDirectory) {
+                    System.out.println("указанный путь является путём к папке");
+                    continue;
+                }
+                System.out.println("Это файл номер " + ++N);
+            }
+         }
 }
